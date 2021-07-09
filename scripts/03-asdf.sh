b1002464ee8fafbf70df84bfb0efec3bf9ca11d8
Ryan Smith <ryan.smith@email.com>
2021-07-09 13:06:56 -0700

    wip ubuntu 20 w/ vnc

--- scripts/03-asdf.sh
+++ scripts/03-asdf.sh
@@ -3,6 +3,7 @@
 set -e
 
 if ! command -v asdf &> /dev/null; then
+  rm -rf ~/.asdf
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf
   cd ~/.asdf
   git checkout "$(git describe --abbrev=0 --tags)"
