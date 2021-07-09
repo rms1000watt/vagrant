006eeef035c63b52651c91407c9dff54b38e93e9
Ryan Smith <ryan.smith@email.com>
2021-07-09 15:15:19 -0700

    wip ubuntu 20 w/ vnc

--- scripts/04-terraform.sh
+++ scripts/04-terraform.sh
@@ -5,6 +5,8 @@ set -e
 terraform_version=1.0.1
 
 if ! command -v terraform &> /dev/null; then
+  source /root/.bashrc
+
   asdf plugin add terraform
   asdf install terraform ${terraform_version}
   asdf global terraform ${terraform_version}
