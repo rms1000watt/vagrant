a826522168725ca8747adb5629ffd45dbbbbec4c
Ryan Smith <ryan.smith@email.com>
2021-07-09 16:12:00 -0700

    wip ubuntu 20 w/ vnc

--- scripts/16-argo-rollouts.sh
+++ scripts/16-argo-rollouts.sh
@@ -4,7 +4,7 @@ set -e
 
 argo_rollouts_version=1.0.1
 
-if ! kubectl argo rollouts -h &> /dev/null; then
+if ! asdf plugin list | grep -q argo-rollouts; then
   asdf plugin add argo-rollouts
   asdf install argo-rollouts ${argo_rollouts_version}
   asdf global argo-rollouts ${argo_rollouts_version}
