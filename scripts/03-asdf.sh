03712bb528099f43b8b66fe736827cee5a0b4e1e
Ryan Smith <ryan.smith@email.com>
2021-07-09 13:22:40 -0700

    wip ubuntu 20 w/ vnc

--- scripts/03-asdf.sh
+++ scripts/03-asdf.sh
@@ -3,12 +3,12 @@
 set -e
 
 if ! command -v asdf &> /dev/null; then
-  rm -rf ~/.asdf
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-  cd ~/.asdf
+  rm -rf /home/vagrant/.asdf
+  git clone https://github.com/asdf-vm/asdf.git /home/vagrant/.asdf
+  cd /home/vagrant/.asdf
   git checkout "$(git describe --abbrev=0 --tags)"
 
-  echo ". \${HOME}/.asdf/asdf.sh" >> ~/.zshrc
+  echo ". /home/vagrant/.asdf/asdf.sh" >> ~/.zshrc
 
   exit 0
 fi
