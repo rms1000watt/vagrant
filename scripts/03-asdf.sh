b1b5123e1ed516273807d34d727ce1155d167704
Ryan Smith <ryan.smith@email.com>
2021-07-09 15:29:21 -0700

    wip ubuntu 20 w/ vnc

--- scripts/03-asdf.sh
+++ scripts/03-asdf.sh
@@ -12,13 +12,6 @@ if [[ ! -f /home/vagrant/.asdf/asdf.sh ]]; then
   echo ". /home/vagrant/.asdf/asdf.sh" >> /home/vagrant/.bashrc
   echo ". /home/vagrant/.asdf/completions/asdf.bash" >> /home/vagrant/.bashrc
 
-  echo ". /home/vagrant/.asdf/asdf.sh" >> /root/.zshrc
-  echo ". /home/vagrant/.asdf/asdf.sh" >> /root/.bashrc
-  echo ". /home/vagrant/.asdf/completions/asdf.bash" >> /root/.bashrc
-
-  source /root/.bashrc
-  source /root/.zshrc
-
   exit 0
 fi
 
