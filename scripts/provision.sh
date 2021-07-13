a70fcd36e06be1099cf9fbb10106536960fd4c26
Ryan Smith <ryan.smith@email.com>
2021-07-12 23:32:39 -0700

    fixed ssh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "NAME=${NAME}"
 
-load_asdf() {
+load_dynamic_deps() {
   if [[ -f /home/vagrant/.asdf/asdf.sh ]]; then
     . /home/vagrant/.asdf/asdf.sh
     export PATH="${PATH}:/home/vagrant/.asdf/shims"
@@ -19,5 +19,5 @@ while read -r file; do
   echo "START: ${file}"
   bash "${file}"
   echo "DONE: ${file}"
-  load_asdf
+  load_dynamic_deps
 done < <(find /scripts -type f | grep -v provision.sh | sort)
