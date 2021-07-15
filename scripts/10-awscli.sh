7aaa391b9b710135e3ebb78ea8b4d50309c35e31
Ryan Smith <ryan.smith@email.com>
2021-07-15 16:55:04 -0700

    update awscli install

--- scripts/10-awscli.sh
+++ scripts/10-awscli.sh
@@ -3,7 +3,10 @@
 set -e
 
 if ! command -v aws &> /dev/null; then
-  python3 -m pip install awscli -U
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm -rf ./aws awscliv2.zip
 
   exit 0
 fi
